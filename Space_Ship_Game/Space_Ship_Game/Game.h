11bdfe18ba2368f880446ed0b6df46469098d4dd
Eoin <c002217717@itcarlow.ie>
2020-01-07 14:23:20 +0000

    Added sfml setup

---
+++ Space_Ship_Game/Space_Ship_Game/Game.h
@@ -0,0 +1,30 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+	
+	void setupFontAndText();
+	void setupSprite();
+
+	sf::RenderWindow m_window; // main SFML window
+
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
+
